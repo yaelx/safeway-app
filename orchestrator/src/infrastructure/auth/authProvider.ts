765da625ad992059f5677f25b27bfc982f64dd45
Yael Sadagurski <yaelisad@gmail.com>
2026-04-02 01:07:20 +0300

    hotfix: authProvider should inherit the identity of the Service Account

--- orchestrator/src/infrastructure/auth/authProvider.ts
+++ orchestrator/src/infrastructure/auth/authProvider.ts
@@ -3,12 +3,9 @@ import { LocalAuthenticator } from "./LocalAuthenticator";
 import dotenv from "dotenv";
 
 dotenv.config();
+const IAM_AUDIENCE = process.env.IAM_AUDIENCE!;
+const isLocal = !!process.env.GOOGLE_APPLICATION_CREDENTIALS;
 
-const IAM_AUDIENCE = process.env.BASE_IAM_URL!;
-export const useRealAuth =
-  process.env.NODE_ENV === "production" ||
-  process.env.GOOGLE_APPLICATION_CREDENTIALS;
-
-export const authProvider = useRealAuth
+export const authProvider = isLocal
   ? new GoogleAuthenticator(IAM_AUDIENCE)
   : new LocalAuthenticator(IAM_AUDIENCE);
