fdcce791b01903b01c33456928c5382f5e6c5a68
Yael Sadagurski <yaelisad@gmail.com>
2026-04-02 00:46:03 +0300

    fixed the authentication- IAM_AUDIENCE is always the base server url. not revision url

--- orchestrator/src/infrastructure/auth/authProvider.ts
+++ orchestrator/src/infrastructure/auth/authProvider.ts
@@ -4,11 +4,11 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const routingServerUrl = process.env.LOGIC_SERVER_URL!;
+const IAM_AUDIENCE = process.env.BASE_IAM_URL!;
 export const useRealAuth =
   process.env.NODE_ENV === "production" ||
   process.env.GOOGLE_APPLICATION_CREDENTIALS;
 
 export const authProvider = useRealAuth
-  ? new GoogleAuthenticator(routingServerUrl)
-  : new LocalAuthenticator(routingServerUrl);
+  ? new GoogleAuthenticator(IAM_AUDIENCE)
+  : new LocalAuthenticator(IAM_AUDIENCE);
