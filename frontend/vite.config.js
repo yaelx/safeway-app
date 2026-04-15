ca2f524fcc3182c91eae2909fb24513a39d7fd4b
Yael Sadagurski <yaelisad@gmail.com>
2026-04-16 00:12:35 +0300

    improved vire config: put big third-party libraries into their own separate files. This helps with browser caching - smaller size app

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,6 +6,20 @@ export default defineConfig({
   build: {
     outDir: "dist", // Explicitly ensure it goes to 'dist'
     emptyOutDir: true,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          // put big third-party libraries into their own separate files. This helps with browser caching
+          if (id.includes("node_modules")) {
+            return id
+              .toString()
+              .split("node_modules/")[1]
+              .split("/")[0]
+              .toString();
+          }
+        },
+      },
+    },
   },
   server: {
     proxy: {
